8ca1de6453aaa387234b0819928203c9f7285ff0
Danny Andrianto <danny_andrianto@hotmail.com>
2013-05-01 01:02:18 +0700

    simplexml

--- get.php
+++ get.php
@@ -1,37 +1,17 @@
 <?php
-// include('./httpful-0.2.0.phar');
-
-// $password = sha1("rezavidi");
-// $uri = "http://phprestsql.ap01.aws.af.cm/members?id>3";
-// // echo $uri . "<br />";
-// $response = \Httpful\Request::get($uri)  // Will parse based on Content-Type
-//     ->expectsXml()              // from the response, but can specify
-//     ->send();                   // how to parse via expectsXml too.
-
-// echo $response->raw_body->{'row'};
-// // if (!isset($response->body->{'row'}))
-// // {
-// // 	echo "fail";
-// // }
-// // else
-// // {
-// // 	echo $response->body->{'row'};
-// // 	echo "success";
-// // }
-
-$xml = simplexml_load_file("http://phprestsql.ap01.aws.af.cm/members?id>1.xml");
-// echo $xml->getName() . "<br>";
-foreach($xml->children() as $child)
-{
-	echo $child->getName() . ": " . $child . "<br>";
-}
-
-// if (!isset($xml->children())) echo 'kosong';
+$xml = simplexml_load_file("http://phprestsql.ap01.aws.af.cm/members/3.xml");
+// if (!isset($xml->row[0]))
+// {
+// 	echo 'FAIL';
+// }
 // else
 // {
-// 	foreach($xml->children() as $child)
-// 	  {
-// 	  echo $child->getName() . ": " . $child . "<br>";
-// 	  }
-// }
+	$i = 0;
+	foreach($xml as $child)
+	{
+		echo $child->getName() . ": " . $child . "<br>";
+		$i++;
+	}
+	echo 'Count:' . $i;
+//}
 ?>
--- src/login.php
+++ src/login.php
@@ -1,37 +1,29 @@
 <?php
-include('./httpful-0.2.0.phar');
-
 $myusername=$_POST['inputusername'];
 $mypassword=sha1($_POST['inputpass']);
 
-$uri = "http://phprestsql.ap01.aws.af.cm/members?username=%22".$myusername."%22&password=%22".$mypassword."%22";
-$response = \Httpful\Request::get($uri)  
-    ->expectsXml()              
-    ->send();                   
-
-if (!isset($response->body->{'row'}))
+$uri = "http://phprestsql.ap01.aws.af.cm/members?username=%22".$myusername."%22&password=%22".$mypassword."%22.xml";
+$xml = simplexml_load_file($uri);
+if (!isset($xml->row[0]))
 {
 	echo "fail";
-	header("location:index.php?status=1");
+	// header("location:index.php?status=1");
 }
 else
 {
-	echo $response->body->{'row'};
 	echo "success";
-	$uri = "http://phprestsql.ap01.aws.af.cm/members/".$response->body->{'row'};
-	$response = \Httpful\Request::get($uri)  
-	    ->expectsXml()              
-	    ->send();
-
+	$uri = "http://phprestsql.ap01.aws.af.cm/members/".$xml->row[0].".xml";
+	$xml = simplexml_load_file($uri);
 	session_start();
 	$_SESSION['myusername'] = $myusername;
-	$_SESSION['id'] = (string)$response->body->{'id'};
-	$_SESSION['fullname'] = (string)$response->body->{'fullname'};
-	$_SESSION['birthdate'] = (string)$response->body->{'birthdate'};
-	$_SESSION['email'] = (string)$response->body->{'email'};
-	$_SESSION['avatar'] = (string)$response->body->{'avatar'};
-	$_SESSION['gender'] = (string)$response->body->{'gender'};
-	$_SESSION['about'] = (string)$response->body->{'about'};
+	$_SESSION['id'] = (string)$xml->id;
+	$_SESSION['fullname'] = (string)$xml->fullname;
+	$_SESSION['birthdate'] = (string)$xml->birthdate;
+	$_SESSION['email'] = (string)$xml->email;
+	$_SESSION['avatar'] = (string)$xml->avatar;
+	$_SESSION['gender'] = (string)$xml->gender;
+	$_SESSION['about'] = (string)$xml->about;
+	echo $_SESSION['myusername'];
 	//header("location:dashboard.php");
 }
 ?>
