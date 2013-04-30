1ed4eaf619a760cabed2ce8a49f1a0b3fc8901e9
13510103 <13510103@std.stei.itb.ac.id>
2013-04-30 21:07:57 +0700

    soap task

--- soap/soap.php
+++ soap/soap.php
@@ -32,6 +32,16 @@ $server->register('registerGan',
         'Register'        
         );
 
+$server->register('taskGan',
+        array('idTaskGan'=>'xsd:string', 'judulTask'=>'xsd:string','creatorTask'=>'xsd:string','deadlineTask'=>'xsd:string','timeTask'=>'xsd:string','timestampTask'=>'xsd:string'),
+        array('return' => 'xsd:string'), 
+        'urn:soap', 
+        'urn:soap#taskGan', 
+        'rpc', 
+        'encoded', 
+        'Task'        
+        );
+
 function hello($name)
 {
 	return 'Hello, '. $name;
@@ -99,7 +109,105 @@ function insertCategory($name, $user, $id)
 	return 'Add category success';
 }
 
+function taskGan($idTaskGan,$judulTask,$creatorTask,$deadlineTask, $timeTask,$timestampTask){
+     include 'database.php';
+    //$creator = $_SESSION['id'];
 
+    $mem = "SELECT * FROM tasks WHERE creator='{$idTaskGan}'";
+        $getMem = mysqli_query($con, $mem);
+        $resulta = mysqli_fetch_array($getMem);
+        $idMember = $resulta['name'];   
+
+    mysqli_query($con, "INSERT INTO tasks (
+                    name,
+                    creator,
+                    deadline,
+                    category,
+                    timestamp
+                    )
+                    VALUES (
+                        '$judulTask',
+                        '$creatorTask',
+                        '$deadline $timeTask',
+                        '$idTaskGan',
+                        '$timestampTask'
+                    )");
+
+    $TaskId = "SELECT id FROM tasks WHERE name='{$judulTask}'";
+    $GetTaskId = mysqli_query($con, $TaskId);
+    $result = mysqli_fetch_array($GetTaskId);
+    $idTask = $result['id'];
+   // echo "<br><br>id task : ".$idTask."<br>";
+
+    $member = array();
+    $member = explode(",", $asignee);
+    $i=1;
+    $j=count($member);
+
+   // echo "banyak asignee : ".$j."<br>";
+
+    while($i<=$j)
+      {
+        $k=$i-1;
+        $member[$k] = trim($member[$k]," ");	
+        $mem = "SELECT * FROM members WHERE username='{$member[$k]}'";
+        $getMem = mysqli_query($con, $mem);
+        $resulta = mysqli_fetch_array($getMem);
+        $idMember = $resulta['id'];
+        echo "<br>nama member : ".$member[$k]." id : ".$idMember;
+        mysqli_query($con, "INSERT INTO assignees (
+                    member,
+                    task,
+                    finished
+                    )
+                    VALUES (
+                        $idMember,
+                        $idTask,
+                        0
+                    )");
+
+        $i++;
+      }
+
+    mysqli_query($con, "INSERT INTO assignees (
+                    member,
+                    task,
+                    finished
+                    )
+                    VALUES (
+                        $creator,
+                        $idTask,
+                        0
+                    )");
+
+    $tagx = array();
+    $tagx = explode(",", $tag);
+    $i=1;
+    $j=count($member);
+    echo "<br><br>banyak tag [new] : ".$j."<br>";
+
+    while($i<=$j)
+      {
+        $k=$i-1;  
+        echo "tag ke-".$i." : ".$tagx[$k]."<br>";
+        mysqli_query($con, "INSERT INTO tags (
+                    name,
+                    tagged
+                    )
+                    VALUES (
+                        '$tagx[$k]',
+                        '$idTask'
+                    )");
+        $i++;
+      }
+
+    mysqli_query($con, "INSERT INTO attachments (path,filetype,task) VALUES ('$direktori','$file','$idTask')");
+//    echo "<br>".$direktori;
+//    echo "<br>".$file;
+//    echo "<br>".$idTask;
+
+    header("location:rinciantugas.php?id=".$TaskId);
+}//end taskGan
 
 
 $POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
--- src/task.php
+++ src/task.php
@@ -1,6 +1,6 @@
 <?php 
 include 'session.php';
-include 'database.php';
+include 'soap.php';
 
 foreach ($_POST as $k => $v) {
 	${$k} = $v;
@@ -24,103 +24,8 @@ move_uploaded_file($source,$direktori);
 
 $creator = $_SESSION['id'];
 
-$mem = "SELECT * FROM tasks WHERE creator='{$id}'";
-	$getMem = mysqli_query($con, $mem);
-	$resulta = mysqli_fetch_array($getMem);
-	$idMember = $resulta['name'];
-	
-echo "nama task : ".$idMember;
+$result = $client->call('taskGan',array('idTaskGan'=>$id, 'judulTask'=>$judul,'creatorTask'=>$creator,'deadlineTask'=>$deadline,'timeTask'=>$time,'timestampTask'=>$timestamp));
 
-mysqli_query($con, "INSERT INTO tasks (
-				name,
-				creator,
-				deadline,
-				category,
-				timestamp
-				)
-				VALUES (
-					'$judul',
-					'$creator',
-					'$deadline $time',
-					'$id',
-					'$timestamp'
-				)");
-
-$TaskId = "SELECT id FROM tasks WHERE name='{$judul}'";
-$GetTaskId = mysqli_query($con, $TaskId);
-$result = mysqli_fetch_array($GetTaskId);
-$idTask = $result['id'];
-echo "<br><br>id task : ".$idTask."<br>";
-
-
-
-$member = array();
-$member = explode(",", $asignee);
-$i=1;
-$j=count($member);
-
-echo "banyak asignee : ".$j."<br>";
-
-while($i<=$j)
-  {
-	$k=$i-1;
-  	$member[$k] = trim($member[$k]," ");	
-	$mem = "SELECT * FROM members WHERE username='{$member[$k]}'";
-	$getMem = mysqli_query($con, $mem);
-	$resulta = mysqli_fetch_array($getMem);
-	$idMember = $resulta['id'];
-	echo "<br>nama member : ".$member[$k]." id : ".$idMember;
-	mysqli_query($con, "INSERT INTO assignees (
-				member,
-				task,
-				finished
-				)
-				VALUES (
-					$idMember,
-					$idTask,
-					0
-				)");
-	
- 	$i++;
-  }
-
-mysqli_query($con, "INSERT INTO assignees (
-				member,
-				task,
-				finished
-				)
-				VALUES (
-					$creator,
-					$idTask,
-					0
-				)");
-
-$tagx = array();
-$tagx = explode(",", $tag);
-$i=1;
-$j=count($member);
-echo "<br><br>banyak tag [new] : ".$j."<br>";
-
-while($i<=$j)
-  {
-	$k=$i-1;  
-	echo "tag ke-".$i." : ".$tagx[$k]."<br>";
-	mysqli_query($con, "INSERT INTO tags (
-				name,
-				tagged
-				)
-				VALUES (
-					'$tagx[$k]',
-					'$idTask'
-				)");
-	$i++;
-  }
-
-mysqli_query($con, "INSERT INTO attachments (path,filetype,task) VALUES ('$direktori','$file','$idTask')");
-echo "<br>".$direktori;
-echo "<br>".$file;
-echo "<br>".$idTask;
-
-header("location:rinciantugas.php?id=".$TaskId);
+//header("location:rinciantugas.php?id=".$TaskId);
 
 ?>
